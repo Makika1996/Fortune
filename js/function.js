c8719718e358049bdf40cdd726a4958017df2219
Makika1996 <marijamarinkov96@gmail.com>
2018-12-12 15:34:25 +0100

    js

--- js/function.js
+++ js/function.js
@@ -5,16 +5,34 @@ $(function() {
       slidesToShow: 6,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 3000
+      autoplaySpeed: 3000,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true
+        }
+      }]
     });
 
     $('.testimonials-slider').slick({
       infinite: true,
       slidesToShow: 3,
-      slidesToScroll: 1,
+      slidesToScroll: 3,
       autoplay: true,
       autoplaySpeed: 3000,
-      dots: true
+      dots: true,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          dots: true,
+        }
+      }]
     });
   });
 });
